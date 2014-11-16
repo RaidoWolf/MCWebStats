7f6ad02e6169fdb2053ba781548118d11e138c0a
Alexander Barber <dangerbarber@gmail.com>
2014-11-16 00:35:32 -0700

    0.5 alpha - added more pretty stats

---
+++ lib/functions_units.php
@@ -0,0 +1,174 @@
+<?php
+
+/*
+ *  MCWebStats Unit Handling Functions
+ *
+ *  FUNCTION LIST:
+ *
+ *  str distance_fancify (double $inputDistance, [str $inputUnit="cm", bool $imperial=false])
+ *  	Convert the value of a specific input unit to another value of an appropriate output value with an appended unit suffix.
+ *  	The returned value is not useable in mathematic operations without stripping the unit suffix.
+ *
+ */
+
+function distance_fancify ($inputDistance, $inputUnit="cm", $imperial=false)
+{
+	$inputUnit = strtolower($inputUnit);
+	if ($inputUnit == "cm")
+	{
+		$thisAsKm = ($inputDistance / 100000);
+		$thisAsM = ($inputDistance / 100);
+		$thisAsMi = ($inputDistance / 160934);
+		$thisAsFt = ($inputDistance / 30.48);
+		$thisAsIn = ($inputDistance / 2.54);
+		if ($imperial == true)
+		{
+			if ($thisAsMi >= 1)
+			{
+				return $thisAsMi."mi";
+			}
+			else
+			{
+				if ($thisAsFt >= 1)
+				{
+					return $thisAsFt."ft";
+				}
+				else
+				{
+					return $thisAsIn."in";
+				}
+			}
+		}
+		elseif ($imperial == false)
+		{
+			if ($thisAsKm >= 1)
+			{
+				return $thisAsKm."km";
+			}
+			else
+			{
+				if ($thisAsM >= 1)
+				{
+					return $thisAsM."m";
+				}
+				else
+				{
+					return $inputDistance."cm";
+				}
+			}
+		}
+		else
+		{
+			$imperialDataType = gettype($imperial);
+			trigger_error("function distance_fancify expected third parameter to be boolean, got $imperialDataType", E_USER_WARNING);
+			return null;
+		}
+	}
+	elseif ($inputUnit == "m")
+	{
+		$thisAsKm = ($inputDistance / 1000);
+		$thisAsCm = ($inputDistance * 100);
+		$thisAsMi = ($inputDistance / 1609.34);
+		$thisAsFt = ($inputDistance * 3.28);
+		$thisAsIn = ($inputDistance * 39.37);
+		if ($imperial == true)
+		{
+			if ($thisAsMi >= 1)
+			{
+				return $thisAsMi."mi";
+			}
+			else
+			{
+				if ($thisAsFt >= 1)
+				{
+					return $thisAsFt."ft";
+				}
+				else
+				{
+					return $thisAsIn."in";
+				}
+			}
+		}
+		elseif ($imperial == false)
+		{
+			if ($thisAsKm >= 1)
+			{
+				return $thisAsKm."km";
+			}
+			else
+			{
+				if ($thisAsM >= 1)
+				{
+					return $inputDistance."m";
+				}
+				else
+				{
+					return $thisAsCm."cm";
+				}
+			}
+		}
+		else
+		{
+			$imperialDataType = gettype($imperial);
+			trigger_error("function distance_fancify expected third parameter to be boolean, got $imperialDataType", E_USER_WARNING);
+			return null;
+		}
+	}
+	elseif ($inputUnit == "km")
+	{
+		$thisAsM = ($inputDistance * 1000);
+		$thisAsCm = ($inputDistance * 100000);
+		$thisAsMi = ($inputDistance / 1.60934);
+		$thisAsFt = ($inputDistance * 3280.84);
+		$thisAsIn = ($inputDistance * 39370.08);
+		if ($imperial == true)
+		{
+			if ($thisAsMi >= 1)
+			{
+				return $thisAsMi."mi";
+			}
+			else
+			{
+				if ($thisAsFt >= 1)
+				{
+					return $thisAsFt."ft";
+				}
+				else
+				{
+					return $thisAsIn."in";
+				}
+			}
+		}
+		elseif ($imperial == false)
+		{
+			if ($inputDistance >= 1)
+			{
+				return $inputDistance."km";
+			}
+			else
+			{
+				if ($thisAsM >= 1)
+				{
+					return $thisAsM."m";
+				}
+				else
+				{
+					return $thisAsCm."cm";
+				}
+			}
+		}
+		else
+		{
+			$imperialDataType = gettype($imperial);
+			trigger_error("function distance_fancify expected third parameter to be boolean, got $imperialDataType", E_USER_WARNING);
+			return null;
+		}
+	}
+	else
+	{
+		trigger_error("function distance_fancify expected second parameter to be a valid unit", E_USER_WARNING);
+		return null;
+	}
+}
+
+?>
--- stats.php
+++ stats.php
@@ -1,3 +1,8 @@
+<?php
+	include_once 'config.php'; //include main configuration
+	include_once 'uuid.php'; //include PHP-GetUUID PlayerName->UUID converter
+	include_once 'lib/functions_units.php'; //include unit handling functions
+?>
 <html>
 <head>
 	<title>MCWebStats - Stats</title>
@@ -5,7 +10,7 @@
 	<link type="text/css" rel="stylesheet" href="css/style.css">
 </head>
 <body>
-<?php #Sputnik One Networks - Minecraft Web Stats - Version 0.4 alpha - stats.php
+<?php #Sputnik One Networks - Minecraft Web Stats - Version 0.5 alpha - stats.php
 if (isset($_REQUEST['player'])) //check if $player variable is set
 	{ //display form to input player info followed by stats as returned by the script
 	echo "<form method='post' action='stats.php'>
@@ -19,8 +24,6 @@ if (isset($_REQUEST['player'])) //check if $player variable is set
 		{$player = $_GET['player'];}
 	else
 		{$player = "none";}
-	include_once 'config.php'; //include main configuration
-	include_once 'uuid.php'; //include PHP-GetUUID PlayerName->UUID converter
 	echo "Player: ".$player."<br />";
 	$playeruuid = GetUUID($player); //use GetUUID function from uuid.php to obtain the UUID from Mojang servers
 	$playeruuid = strtolower($playeruuid); //convert any and all capital letters to lowercase
@@ -41,10 +44,10 @@ if (isset($_REQUEST['player'])) //check if $player variable is set
 	if (file_exists("$mcroot/world/stats/$playeruuid.json")) #test if we have stats for that player.
 		{
 		$stats = file_get_contents("$mcroot/world/stats/$playeruuid.json"); #read stats file contents.
-		$json_decode = json_decode($stats,true);
+		$playerStatArray = json_decode($stats,true);
 
 		// -- TIME PLAYED DATA -- //
-		$ticksPlayed = $json_decode['stat.playOneMinute']; //get number of ticks played
+		$ticksPlayed = $playerStatArray['stat.playOneMinute']; //get number of ticks played
 			$secondsPlayed = ($ticksPlayed / 20); //number of seconds played
 			$minutesPlayed = ($ticksPlayed / 1200); //number of minutes played
 			$hoursPlayed = ($ticksPlayed / 72000); //number of hours played
@@ -55,14 +58,27 @@ if (isset($_REQUEST['player'])) //check if $player variable is set
 			$hoursModuloDaysPlayed = (($ticksPlayed % 1728000) / 72000); //number of hours left over after days floored (modulo) 
 		$timePlayed = floor($daysPlayed)."d ".floor($hoursModuloDaysPlayed)."h ".floor($minutesModuloHoursPlayed)."m ".$secondsModuloMinutesPlayed."s";
 
-		// -- DISTANCE WALKED DATA -- //
-		$cmWalked = $json_decode['stat.walkOneCm']; //get number of centimeters walked
-			$kmWalked = ($cmWalked / 100000); //number of kilometers walked
-			$mWalked = ($cmWalked / 100); //number of meters walked
-			$miWalked = ($cmWalked / 160934); //number of miles walked
-			$ydWalked = ($cmWalked / 91.44); //number of yards walked
-			$ftWalked = ($cmWalked / 30.48); //number of centimeters walked
-			$inWalked = ($cmWalked / 2.54); //number of inches walked
+		// -- DISTANCE TRAVELED DATA -- //
+		$cmWalked = $playerStatArray['stat.walkOneCm']; //get number of centimeters walked
+			$distWalked = distance_fancify($cmWalked, "cm", false); //convert distance walked to human-readable format
+		$cmSprinted = $playerStatArray['stat.sprintOneCm']; //get number of centimeters sprinted
+			$distSprinted = distance_fancify($cmSprinted, "cm", false); //convert distance sprinted to human-readable format
+		$cmCrouched = $playerStatArray['stat.crouchOneCm']; //get number of centimeters sneaking/crouched
+			$distCrouched = distance_fancify($cmCrouched, "cm", false); //convert distance sneaking/crouched to human-readable format
+		$cmClimbed = $playerStatArray['stat.climbOneCm']; //get number of centimeters climbed
+			$distClimbed = distance_fancify($cmClimbed, "cm", false); //convert distance climbed to human-readable format
+		$cmFallen = $playerStatArray['stat.fallOneCm']; //get number of centimeters fallen
+			$distFallen = distance_fancify($cmFallen, "cm", false); //convert distance fallen to human-readable format 
+		$cmBoated = $playerStatArray['stat.boatOneCm']; //get number of centimeters boated
+			$distBoated = distance_fancify($cmBoated, "cm", false); //convert distance boated to human-readable format
+		$cmSwam = $playerStatArray['stat.swimOneCm']; //get number of centimeters swam
+			$distSwam = distance_fancify($cmSwam, "cm", false); //convert distance swam to human-readable format
+		$cmDived = $playerStatArray['stat.diveOneCm']; //get number of centimeters dived
+			$distDived = distance_fancify($cmDived, "cm", false); //convert distance dived to human-readable format
+		$cmHorse = $playerStatArray['stat.horseOneCm']; //get number of centimeters ridden on a horse
+			$distHorse = distance_fancify($cmHorse, "cm", false); //convert distance ridden on a horse to human-readable format
+		$cmFlown = $playerStatArray['stat.flyOneCm']; //get number of centimeters flown/launched
+			$distFlown = distance_fancify($cmFlown, "cm", false); //convert distance flown/launched to human-readable format
 
 		echo "
 		<table>
@@ -79,13 +95,76 @@ if (isset($_REQUEST['player'])) //check if $player variable is set
 					<p>Distance Walked:</p>
 				</td>
 				<td>
-					<p>".$kmWalked."km</p> 
+					<p>".$distWalked."</p> 
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<p>Distance Sprinted:</p>
+				</td>
+				<td>
+					<p>".$distSprinted."</p>
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<p>Distance Sneaking:</p>
+				</td>
+				<td>
+					<p>".$distCrouched."</p>
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<p>Distance Climbed:</p>
+				</td>
+				<td>
+					<p>".$distClimbed."</p>
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<p>Distance Fallen:</p>
+				</td>
+				<td>
+					<p>".$distFallen."</p>
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<p>Distance Swam:</p>
+				</td>
+				<td>
+					<p>".$distSwam."</p>
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<p>Distance Underwater:</p>
+				</td>
+				<td>
+					<p>".$distDived."</p>
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<p>Distance on Horse:</p>
+				</td>
+				<td>
+					<p>".$distHorse."</p>
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<p>Distance Airborne</p>
+				</td>
+				<td>
+					<p>".$distFlown."</p>
 				</td>
 			</tr>
 		</table>
 		";
-		$result_printr = print("<pre>".print_r($json_decode,true."</pre>"));
-		//extract($result_printr, EXTR_PREFIX_ALL, "stat");
+		$result_printr = print("<pre>".print_r($playerStatArray,true."</pre>"));
 		echo $result_printr;
 		}
 	else
@@ -94,8 +173,8 @@ if (isset($_REQUEST['player'])) //check if $player variable is set
 		if (file_exists("$mcroot/$mcworld/stats/$player.json")) #Legacy support
 			{
 			$stats = file_get_contents("$mcroot/$mcworld/stats/$player.json");
-			$json_decode = json_decode($stats, true);
-			$result_printr = print("<pre>".print_r($json_decode, true."</pre>"));
+			$playerStatArray = json_decode($stats, true);
+			$result_printr = print("<pre>".print_r($playerStatArray, true."</pre>"));
 			//extract($result_printr, EXTR_PREFIX_ALL, "stat");
 			echo $result_printr;
 			}
